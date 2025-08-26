fa88e8c22c378d72c7d5000497089995662d29aa
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-08-26 22:29:45 +0700

    feat(models): add Company and Departement models with relationships and fillable attributes

---
+++ app/Models/Company.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'email',
+    ];
+
+    public function departments()
+    {
+        return $this->hasMany(Departement::class);
+    }
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
---
+++ app/Models/Departement.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Departement extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'company_id',
+        'token',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
