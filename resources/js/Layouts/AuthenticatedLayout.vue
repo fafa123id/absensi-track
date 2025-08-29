91b8ef34197d7a7e33ec6b18ede7346b45826050
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-08-29 18:44:02 +0700

    refactor(layout): clean up code formatting in AuthenticatedLayout.vue

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,23 +1,34 @@
 <script setup>
-import NavbarLayout from '@/Layouts/NavbarLayout.vue';
-import { usePage } from '@inertiajs/vue3';
-import { watch } from 'vue';
+import NavbarLayout from "@/Layouts/NavbarLayout.vue";
+import { usePage } from "@inertiajs/vue3";
+import { watch } from "vue";
 const page = usePage();
 import { confirmAction, showSuccess, showError } from "@/Composables/swal";
 
-watch(() => page.props.flash.success, (newMessage) => {
-    if (newMessage) {
-        showSuccess('Berhasil!', newMessage);
-        page.props.flash.success = null;
+watch(
+    () => page.props.flash.success,
+    (newMessage) => {
+        if (newMessage) {
+            showSuccess("Berhasil!", newMessage);
+            page.props.flash.success = null;
+        }
+    },
+    {
+        immediate: true,
     }
-});
-watch(() => page.props.flash.error, (newMessage) => {
-    if (newMessage) {
-        showError('Gagal!', newMessage);
-        page.props.flash.error = null;
+);
+watch(
+    () => page.props.flash.error,
+    (newMessage) => {
+        if (newMessage) {
+            showError("Gagal!", newMessage);
+            page.props.flash.error = null;
+        }
+    },
+    {
+        immediate: true,
     }
-});
-
+);
 </script>
 
 <template>
@@ -25,10 +36,7 @@ watch(() => page.props.flash.error, (newMessage) => {
         <div class="min-h-screen bg-gray-100">
             <NavbarLayout />
             <!-- Page Heading -->
-            <header
-                class="bg-white shadow"
-                v-if="$slots.header"
-            >
+            <header class="bg-white shadow" v-if="$slots.header">
                 <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     <slot name="header" />
                 </div>
