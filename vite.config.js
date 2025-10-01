884562fc0abc3cc15481543175ae1bde432fc485
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-10-01 14:58:56 +0700

    feat(dependencies): add lodash.debounce package for improved debouncing functionality

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+const localIp = '192.168.1.7'; 
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: localIp,
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
