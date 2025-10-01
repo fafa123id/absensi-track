ec30d0029e36349b9101f7997b8ecb6f1e1cdf23
fafa123id <104996246+fafa123id@users.noreply.github.com>
2025-10-01 15:00:22 +0700

    fix(dashboard): update index method to accept Request parameter and adjust departements query

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,23 +9,23 @@ use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $company = auth()->user()->company()->with('departements')->firstOrFail();
         $authUserId = auth()->id();
-        $departements = Departement::where('company_id', $company->id)
+        $departements = Departement::query()->where('company_id', $company->id)
             ->with([
                 'users' => function ($query) use ($authUserId) {
                     $query->where('id', '!=', $authUserId);
                 }
             ])
-            ->orderBy('id')->paginate(5);
+            ->orderBy('id')->get();
 
         return Inertia::render(
             'Dashboard',
             [
                 'departements' => $departements,
-                'company' => $company
+                'company' => $company,
             ]
         );
     }
