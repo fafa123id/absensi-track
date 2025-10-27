36406b1bbb021f0f0d114dc94474fac160b12118
fafa123id <ahmad.fauzan.rozikin@gmail.com>
2025-10-27 09:24:55 +0700

    chore: update dependencies and configure Vite for local development
    - Added new devDependencies: @laravel/echo-vue, laravel-echo, pusher-js, and vite-plugin-mkcert.
    - Added new dependency: @chenfengyuan/vue-qrcode and vue-qrcode-reader.
    - Updated Vite configuration to include mkcert plugin and commented out server settings for local development.

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,39 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-const localIp = '192.168.1.7'; 
-
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import mkcert from "vite-plugin-mkcert";
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: localIp,
-        },
-    },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
+            // fullUrl: true,
         }),
         vue({
             template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+                transformAssetUrls: { base: null, includeAbsolute: false },
             },
         }),
+        // mkcert TIDAK diaktifkan dulu supaya semua tetap HTTP
+        // mkcert(), // aktifkan kalau semua sudah HTTPS
     ],
+    // server: {
+    //     host: true, // atau "0.0.0.0"
+    //     port: 5173,
+    //     strictPort: true,
+    //     hmr: {
+    //         host: "10.126.254.68",
+    //         port: 5173,
+    //         protocol: "ws", // pakai "wss" hanya kalau HTTPS
+    //     },
+    // },
+    // server: {
+    //     // Cukup ini. Biarkan Vite bisa diakses dari jaringan lokal.
+    //     host: '0.0.0.0',
+    // },
+    // server: {
+    //     host: "0.0.0.0",
+    //     https: true, // WAJIB: Paksa Vite berjalan di HTTPS
+    //     port: 5173,
+    // },
 });
